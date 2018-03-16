6a7533d3d45b64d9675204f51d73f5a661a599c4
Magda <magdtrag@gmail.com>
2018-03-16 19:57:13 +0100

    Initial Message

---
+++ src/Interview.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Interview {
+
+  static void interview(){
+      String buffer;
+
+      Scanner odczyt = new Scanner(System.in);
+      buffer = odczyt.nextLine();
+      System.out.println("Witaj "+buffer);
+
+  }
+
+  void askForAlternatives(){
+
+  }
+
+
+
+
+}
