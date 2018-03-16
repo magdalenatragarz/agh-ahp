ca6e211768fcaf7a8b3db82c4d6d51a9291aacdc
Magda <magdtrag@gmail.com>
2018-03-16 23:22:16 +0100

    Added JSON library

--- src/Interview.java
+++ src/Interview.java
@@ -1,12 +1,16 @@
+import org.json.JSONObject;
+
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 class Interview {
+    JSONObject jo = new JSONObject();
+    private ArrayList<String> alternatives = new ArrayList<>();
+    private ArrayList<String> criterias = new ArrayList<>();
 
-    static private ArrayList<String> alternatives = new ArrayList<>();
-
-  static void interview(){
+   void interview(){
       String buffer;
 
 
@@ -15,11 +19,11 @@ class Interview {
 
   }
 
-  static void introduction(){
+  void introduction(){
       System.out.println("======Analytic Hierarchy Process======");
   }
 
-  static void askForAlternatives() {
+  void askForAlternatives() {
       String alternativeBuffer;
       int numberOfAternatives;
       System.out.println("How many alternatives does solution of your problem have?");
@@ -27,17 +31,62 @@ class Interview {
       numberOfAternatives = scanner.nextInt();
       if (numberOfAternatives > 0) {
           System.out.println("Enter set of yours alternatives, press ENTER after each.");
-          for (int i = 0; i <= numberOfAternatives; i++) {
+          for (int i=0; i<=numberOfAternatives; i++) {
               alternativeBuffer = scanner.nextLine();
               alternatives.add(alternativeBuffer);
           }
+      }else{
+          System.out.println("Nothing to choose :(");
       }
   }
+
+  void lookforCriterias(){
+      String criteriaBuffer;
+      int numberOfCriterias;
+
+      System.out.println("How many criterias do you have?");
+      Scanner scanner = new Scanner(System.in);
+      numberOfCriterias = scanner.nextInt();
+      if (numberOfCriterias>0){
+          System.out.println("Enter set of criterias, press ENTER after each.");
+          for (int i=0;i<=numberOfCriterias;i++){
+              criteriaBuffer = scanner.nextLine();
+              if(!criteriaBuffer.equals("")) {
+                  criterias.add(criteriaBuffer);
+                  lookForSubcriterias(criteriaBuffer);
+              }
+          }
+      }
+  }
+
+  void lookForSubcriterias(String criteria){
+
+      String permission;
+      int numberOfSubcriteria;
+      String subcriteriaBuffer;
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Does criteria "+criteria+" have subcriteria? [y/n]");
+      permission = scanner.nextLine();
+      if(permission.equals("y")){
+          System.out.println("How many subcriterias of "+criteria+" do you have?");
+              numberOfSubcriteria=scanner.nextInt();
+              if (numberOfSubcriteria>0) {
+                  System.out.println("Enter set of subcriterias of "+criteria+", press ENTER after each.");
+                  for (int i = 0; i < numberOfSubcriteria; i++) {
+                      subcriteriaBuffer = scanner.nextLine();
+                      criterias.add(subcriteriaBuffer);
+                  }
+              }
+      }else{
+
+      }
+  }
+
       //=======help==========
 
-    static void show(){
+    void show(){
       //String temp="";
-        for (String temp:alternatives){
+        for (String temp:criterias){
           System.out.println(temp);
       }
     }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,56 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import java.io.PrintWriter;
+
 public class Main {
 
     public static void main(String[] args){
-        Interview.introduction();
-        Interview.askForAlternatives();
-        Interview.show();
+        JSONObject jo = new JSONObject();
+
+        jo.put("firstName", "John");
+        jo.put("lastName", "Smith");
+        jo.put("age", 25);
+
+        // for address data, first create LinkedHashMap
+        Map m = new LinkedHashMap(4);
+        m.put("streetAddress", "21 2nd Street");
+        m.put("city", "New York");
+        m.put("state", "NY");
+        m.put("postalCode", 10021);
+
+        // putting address to JSONObject
+        jo.put("address", m);
+
+        // for phone numbers, first create JSONArray
+        JSONArray ja = new JSONArray();
+
+        m = new LinkedHashMap(2);
+        m.put("type", "home");
+        m.put("number", "212 555-1234");
+
+        // adding map to list
+        ja.put(m);
+
+        m = new LinkedHashMap(2);
+        m.put("type", "fax");
+        m.put("number", "212 555-1234");
+
+        // adding map to list
+        ja.put(m);
+
+        // putting phoneNumbers to JSONObject
+        jo.put("phoneNumbers", ja);
+
+        // writing JSON to file:"JSONExample.json" in cwd
+        try {
+            PrintWriter pw = new PrintWriter("JSONExample.json");
+            pw.write(jo.toString());
+        }catch (Exception e){
+            System.out.println("koniec");
+        }
     }
 
-}
+ }
